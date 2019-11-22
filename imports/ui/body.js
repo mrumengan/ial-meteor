c4869bfe27f8fc4f3a0fbf47ee68ddadb37f8918
Mason Rumengan <masonrumengan@yahoo.com>
2019-11-22 21:00:20 +0700

    [update]

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,18 @@
 import { Template } from 'meteor/templating';
- 
+
+import { Tasks } from '../api/tasks.js';
+
 import './body.html';
  
 Template.body.helpers({
   tasks: [
-    { text: 'This is task 1' },
-    { text: 'This is task 2' },
-    { text: 'This is task 3' },
+    { text: 'Daftar Gawai' },
+    { text: 'Daftar Gawai Berdasarkan Lokasi' },
   ],
+});
+
+Template.task.events({
+  'click': function(){
+      console.log("You clicked something");
+  }
 });
